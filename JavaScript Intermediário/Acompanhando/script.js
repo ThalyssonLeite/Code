f42486418aca6bd9b71e75e107ca547877831c57
Thalysson Leite <thalyssonl17@gmail.com>
2021-05-29 11:46:15 -0300

    daily update

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -2,3 +2,8 @@ function changeH1(userInput) {
     let h1 = document.getElementsByTagName("h1")[0]
     h1.innerText = userInput.value
 }
+
+function hideH1() {
+    let h1 = document.getElementsByTagName("h1")[0];
+    h1.style = "display: none;"
+}
