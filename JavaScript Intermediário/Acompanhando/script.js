cb4da539fce80ab8045b80c89771f26af2972256
Thalysson Leite <thalyssonl17@gmail.com>
2021-05-29 12:45:43 -0300

    merge commit

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,16 +1,5 @@
-function criarAluno(name, n1, n2) {
-   
-    return {
-        nome: name,
-        nota1: n1,
-        nota2: n2,
-        media: function () {
-             return (this.nota1 + this.nota2) / 2;
-        }
-    }
-   
-}
+function changeH1() {
+    let h1 = document.body.getElementsByTagName("h1")
 
-var aluno = criarAluno("Thalysson", 9, 10);
-console.log(aluno)
-console.log(aluno.nome + " - " + aluno.media());
+    h1.style = "color:red";
+}
