7b9daf3d7c74f45f2b320c242e3abd4781374a4a
Thalysson Leite <thalyssonl17@gmail.com>
2021-05-30 17:34:21 -0300

    Math object and some pretty little self explanatory explanations

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,17 +1,17 @@
-window.onload = function() {
-    let h1 = document.getElementsByTagName("h1");
-    
-    for (let h of h1) {
-        console.log(h);
+window.onload = function () {
+    let pi = Math.PI;
+    let squareRoot = Math.sqrt(16);
+    let power = Math.pow(2, 3);
+    let randomNumberFromOneToTen = Math.random() * 10;
+    let roundAFloatNumber = Math.round(Math.random() * 10) //Math.round(5.5) -> 6
+    let putThatNumberToTheFloor = Math.floor(5.99);
+    let putThatNumberToTheCeil = Math.ceil(5.000001);
 
-        h.addEventListener("click", whoIsTheFather);
-    }
-
-    function whoIsTheFather(e) {
-        let target = e.target;
-        let parent = target.parentElement;
-
-        console.log(parent);
-    }
+    console.log(pi);
+    console.log(squareRoot);
+    console.log(power);
+    console.log(randomNumberFromOneToTen);
+    console.log(roundAFloatNumber);
+    console.log(putThatNumberToTheFloor);
+    console.log(putThatNumberToTheCeil);
 }
-
