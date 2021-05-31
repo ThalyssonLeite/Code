405676b56d8bc8d5ca2087412bd6b7e9a7cccd20
Thalysson Leite <thalyssonl17@gmail.com>
2021-05-30 21:27:33 -0300

    Seeing JSON for the first time (and pretending that is that simple...)

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,17 +1,19 @@
-window.onload = function () {
-    let pi = Math.PI;
-    let squareRoot = Math.sqrt(16);
-    let power = Math.pow(2, 3);
-    let randomNumberFromOneToTen = Math.random() * 10;
-    let roundAFloatNumber = Math.round(Math.random() * 10) //Math.round(5.5) -> 6
-    let putThatNumberToTheFloor = Math.floor(5.99);
-    let putThatNumberToTheCeil = Math.ceil(5.000001);
-
-    console.log(pi);
-    console.log(squareRoot);
-    console.log(power);
-    console.log(randomNumberFromOneToTen);
-    console.log(roundAFloatNumber);
-    console.log(putThatNumberToTheFloor);
-    console.log(putThatNumberToTheCeil);
+let student1 = {
+    name: "Igor",
+    score: 8.5
 }
+
+let student2 = '{"name": "Fábio", "score": 7.8}'
+
+let b1 = JSON.stringify(student1);
+let b2 = JSON.parse(student2);
+
+console.log(student1);
+console.log(student2);
+console.log(b1);
+console.log(b2) 
+
+console.log(b1.name);
+console.log(b1.score);
+console.log(b2.name);
+console.log(b2.score);
