6f32e65b5803f2ab96d1af568496abe7e16af14c
Thalysson Leite <thalyssonl17@gmail.com>
2021-05-30 22:38:32 -0300

    Local storage persistence from input tag

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,19 +1,16 @@
-let student1 = {
-    name: "Igor",
-    score: 8.5
+onload = function() {
+    let h1 = document.getElementsByTagName("h1")[0];
+    let name = localStorage.getItem("name");
+
+    h1.innerHTML = name
 }
 
-let student2 = '{"name": "Fábio", "score": 7.8}'
+function update(element) {
+    let h1 = document.getElementsByTagName("h1")[0]
+    let userInput = element.value
 
-let b1 = JSON.stringify(student1);
-let b2 = JSON.parse(student2);
+    h1.innerHTML = userInput
 
-console.log(student1);
-console.log(student2);
-console.log(b1);
-console.log(b2) 
-
-console.log(b1.name);
-console.log(b1.score);
-console.log(b2.name);
-console.log(b2.score);
+    localStorage.setItem("name", userInput)
+    
+}
