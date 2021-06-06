ddf34d916eba0eedc108216011b9641880a92103
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-06 13:58:38 -0300

    adding a thumbsDown

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,20 +1,2 @@
-let number = 0;
-let interval;
-
-function start() {
-    let h1 = document.getElementById("title");
-    interval = setInterval(changeTitle, 1000)
-    if (!(h1.innerText.includes("New"))) {
-        h1.innerHTML = "New title"
-    }
-}
-
-function stop() {   
-    clearInterval(interval);
-}
-
-function changeTitle() {
-    let h1 = document.getElementById("title");
-    h1.innerHTML += " " + number;
-    number++;
-}
+let image = document.getElementById("like");
+addEventListener("click", changeImage)
