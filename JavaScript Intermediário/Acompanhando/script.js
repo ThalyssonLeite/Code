33bba3f99cfe735af013115705d89560195ec8db
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-06 17:56:59 -0300

    get and set Attribute

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,2 +1,9 @@
 let image = document.getElementById("like");
-addEventListener("click", changeImage)
+image.addEventListener("click", changeImage);
+
+function changeImage() {
+    let firstImage = image.getAttribute("src");
+    image.setAttribute("src", "./imagens/thumbsDown.png"); 
+
+    console.log(firstImage)
+}
