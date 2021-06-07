f5af897c3be5de6ac78fc4deda59152b0fcc84da
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-07 20:57:41 -0300

    all the confusing and unnecessary types and ways to do rectangles in the canvas tag

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,10 +1,32 @@
-let screen = document.getElementById("screen");
-let context = screen.getContext("2d");
-context.moveTo(0, 0);
-context.lineTo(250, 250);
-context.lineTo(500, 0);
-// The methods that play with the "style" and  can be setted anywhere, as long as it stays behind the stroke() function.
-context.lineWidth = 5;
-context.strokeStyle = "blue";
-// Just one stroke() function is required to do the job.
-context.stroke();
+let canvas = document.getElementById("canvas");
+
+let canvasContext = canvas.getContext("2d");
+
+
+const blueSquare = () => {
+canvasContext.fillStyle = "blue";
+
+// In general, the first two arguments represent the X and Y (axis) padding and the other two arguments that comes after them represents the normal width and height properties.
+canvasContext.fillRect(10, 10, 200, 200);
+}
+
+const drawnBlueSquare = () => {
+canvasContext.strokeStyle = "blue";
+canvasContext.lineWidth = 10;
+canvasContext.strokeRect(220, 10, 200, 200);
+}
+
+const bordedBlueSquare = () => {
+canvasContext.rect(15, 230, 200, 200);
+canvasContext.fillStyle = "blue";
+canvasContext.lineWidth = 10;
+canvasContext.strokeStyle = "cornflowerblue";
+canvasContext.fill();
+canvasContext.stroke();
+}
+
+bordedBlueSquare();
+blueSquare();
+drawnBlueSquare();
+
+canvasContext.clearRect(30, 30, 50, 50);
