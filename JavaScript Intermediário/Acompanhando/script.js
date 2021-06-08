f3189600f771d26162c318ba9e7eed38b3e22c77
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-07 21:44:52 -0300

    just getting back to the last commit

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,10 +1,9 @@
-let screen = document.getElementById("screen");
-let context = screen.getContext("2d");
-context.moveTo(0, 0);
-context.lineTo(250, 250);
-context.lineTo(500, 0);
-// The methods that play with the "style" and  can be setted anywhere, as long as it stays behind the stroke() function.
-context.lineWidth = 5;
-context.strokeStyle = "blue";
-// Just one stroke() function is required to do the job.
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+
+context.beginPath();
+
+context.strokeStyle = "red";
+context.arc(250, 250, 25, 0, 2 * Math.PI)
 context.stroke();
