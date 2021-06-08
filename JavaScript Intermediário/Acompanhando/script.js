35a2f724045d5dc3de5073efb22b02db381d2e3e
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-07 22:36:44 -0300

    setting an image in canvas through JavaScript

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
-let img = document.getElementsByTagName("img")[0];
+let img = new Image();
+img.src = "./assets/images/thumbsUp.png";
 
-context.drawImage(img, 20, 20);
+img.onload = () => context.drawImage(img, 50, 50, img.naturalWidth/2, img.naturalHeight/2);
