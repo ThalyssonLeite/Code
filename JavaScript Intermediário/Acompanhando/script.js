8bad5150104927d6afb15aaa5490f7fd974a1a2d
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-09 15:36:34 -0300

    I do need to learn getComputedStyle and querySelector and getPropertyValue... I am learning pseudoElements those: h1::something

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,7 +1,2 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
-
-let img = new Image();
-img.src = "./assets/images/thumbsUp.png";
-
-img.onload = () => context.drawImage(img, 50, 50, img.naturalWidth/2, img.naturalHeight/2);
+let color = getComputedStyle(document.querySelector('p'), '::selection').getPropertyValue('background-color')
+console.log(color)
