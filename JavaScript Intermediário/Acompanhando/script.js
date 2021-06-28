dd90442ca443b7277725ae48711c2795f23c16f2
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-27 21:19:58 -0300

    async function and await

--- JavaScript Intermediário/Acompanhando/script.js
+++ JavaScript Intermediário/Acompanhando/script.js
@@ -1,17 +1,32 @@
-function speakGeneric() {
-    console.log(this.sound)
+let users = ["Adriano", "Marcia", "Jose"];
+
+function insertUser(name) {
+
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            users.push(name);
+
+            let error = 0;
+
+            if (!error) {
+                resolve()
+            } else {
+                reject({msg: "Something wrong is not right"})
+            }
+
+        }, 500)
+    })
+
+    return promise
 }
 
-let dog = {
-    sound: "ow",
-    speak: speakGeneric
+function printUsers() {
+    console.log(users)
 }
 
-let cat = {
-    sound: "miaw",
-    speak: speakGeneric
+async function execute() {
+    await insertUser("Thalysson");
+    printUsers()
 }
 
-let bindedFunction = speakGeneric.bind(dog);
-
-bindedFunction();
+execute()
