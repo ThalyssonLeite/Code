962d8f153d73b1040ddc29f1172891047d7037e1
Thalysson Leite <thalyssonl17@gmail.com>
2021-07-03 15:30:58 -0300

    Adding some node_modulesand yarn stuff... I learned process.stdin.on() and process.on() and process.exit()

--- JavaScript Intermediário/Acompanhando/index.js
+++ JavaScript Intermediário/Acompanhando/index.js
@@ -1,4 +1,5 @@
 const questions = [
+    "Como você se chama",
     "O que aprendi hoje?",
     "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
     "O que me deixou feliz hoje?",
@@ -6,7 +7,42 @@ const questions = [
 ]
 
 const ask = ( index = 0 ) => {
-    process.stdout.write(questions[index]);
+    process.stdout.write("\n\n" + questions[index] + " > ");
 }
 
 ask();
+
+const answers = [];
+
+process.stdin.on("data", (data) => {
+    answers.push(data.toString().trim() + "\n")
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        for (string of answers) {
+            let index = answers.indexOf(string);
+            answers[index] = string.slice(0, -1);
+        }
+        console.log(answers);
+        process.exit()
+    }
+})
+
+process.on("exit", () => {
+    process.stdout.write(
+        `Bacana ${answers[0]}!
+
+        O que você aprendeu hoje foi:
+        ${answers[1]}
+
+        O que te aborreceu e você poderia melhorar foi:
+        ${answers[2]}
+
+        O que te deixou feliz hoje:
+        ${answers[3]}
+
+        Você ajudou ${answers[4]} pessoas hoje!
+
+        Volte amanhã para mais reflexões.`
+    )
+})
